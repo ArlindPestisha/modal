69d2b6249def6fbc3c8336c2803289aa294dec89
ArlindPestisha <55292668+ArlindPestisha@users.noreply.github.com>
2020-11-25 07:16:59 +0100

    Building the logic to show the modals affter clicking the buttons

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+// Selecting all the elements that we need to use
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+console.log(btnsOpenModal);
+
+// Using for loop for the btnModals
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', function() {
+        console.log('Btn is Clicked');
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+});
